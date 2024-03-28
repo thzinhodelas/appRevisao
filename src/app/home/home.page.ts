955f948ce4adf2202d5220d8bda172495734bbe1
theodoro <theodoro.silva@aluno.grupodombosco.com.br>
2024-03-28 16:51:13 -0300

    Fim do Projeto Revisão

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  n1='';
+  n2='';
+  res=0;
 
+  constructor(private alertController: AlertController) {}
+
+  async alertaDivisaoPorZero() {
+    const alert = await this.alertController.create({
+      header: 'Ops...',
+      message: 'Não é possível dividir por zero',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  somar() {
+  this.res=parseFloat(this.n1)+parseFloat(this.n2);
+  }
+
+  subtrair() {
+    this.res=parseFloat(this.n1)-parseFloat(this.n2);
+    }
+
+  multiplicar() {
+    this.res=parseFloat(this.n1)*parseFloat(this.n2);
+    }
+
+  dividir() {
+    if (parseFloat(this.n2)==0){
+      this.res=0;
+      this.alertaDivisaoPorZero();
+    }else{
+      this.res=parseFloat(this.n1)/parseFloat(this.n2);
+    }
+    }
 }
